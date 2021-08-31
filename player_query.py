98c16cae20c644c3bf313f7547a081bd1422a05b
Aahil Samnani <aahilsamnani@Aahils-MacBook-Air.local>
2021-08-31 13:44:58 -0400

    Added comments and implemented post/redirect/get pattern

--- player_query.py
+++ player_query.py
@@ -11,11 +11,7 @@ import requests
 import pandas as pd
 import numpy as np
 import sqlite3
-from sqlalchemy import text, create_engine
-
-engine = create_engine("sqlite:///soccer.db")
-con = engine.connect()
-results = con.execute(text("SELECT * FROM transfers"))[0]
+import psycopg2
 
 
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
@@ -42,6 +38,58 @@ df = elements_df[["name", "web_name", "position", "team", "total_points",
 player_info = df[["name", "web_name", "position", "team", "now_cost", 
                                        "form", "news", "news_added", "total_points"]]
 
+
+"""if request.method == "POST":
+        # Normalize string, unidecode removes accents
+        player_input = unidecode(request.form.get("player").strip().lower())
+        if not player_input:
+            return redirect("/players")
+        # Remove all accents from player name stored in DataFrame
+        player_df_row = df.loc[df["name"].apply(unidecode).str.lower().str.contains(player_input)]
+
+        if len(player_df_row) == 1:
+            player = player_df_row["name"].iloc[0]
+            player_info = player_df_row[["name", "web_name", "position", "team", "now_cost", 
+                                       "form", "news", "news_added", "total_points"]]
+            player_stats = player_df_row.iloc[:, 8:]
+
+            player_index = player_info.index[0]
+            player_dict = player_info.to_dict(orient="index")[player_index]
+
+            # Get player code to retreive image of player from soccer API in player.html
+            player_dict["code"] = list(elements_df["code"].loc[elements_df["name"] == player])[0]
+
+            if player_dict["news"]:
+                player_dict["news"] = player_dict["news"]
+                # Exctract news date from timestamp
+                player_dict["news_added"] = player_dict["news_added"][0:10]
+
+            player_stats.columns = player_stats.columns.str.title().str.replace("_", " ")
+            player_stats_table = Markup(player_stats.to_html(index=False))
+          
+            con = sqlite3.connect(db)
+            cur = con.cursor()
+            # Check if player is in team to decide to display either an "add" or "delete" player button in player.html
+            player_row = cur.execute("SELECT * FROM team WHERE name = ? AND user_id = ?", (player_dict["name"], session["user_id"])).fetchall()
+            if len(player_row) == 1:
+                player_in_team = True
+            else:
+                 player_in_team = False
+            cur.execute("INSERT INTO searches (user_id, name, web_name, position, team, now_cost, form, points) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", 
+                        (session["user_id"], player_dict["name"], player_dict["web_name"], player_dict["position"], player_dict["team"],
+                         player_dict["now_cost"], player_dict["form"], player_dict["total_points"]))
+            con.commit()
+            con.close()
+            
+            # Render another page to show player profile
+            return render_template("player.html", player_dict=player_dict, player_stats_table=player_stats_table,
+                                   player_in_team=player_in_team)
+
+        return redirect("/players")
+
+    else:
+        players_df = df[["name", "position", "team"]]
+        return render_template("players.html", players_df=players_df)"""
 
 
 
